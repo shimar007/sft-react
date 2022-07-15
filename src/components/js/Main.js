41186a0c20961bb074c8498e28f91cb6b57d3852
Shiva <shimar007@gmail.com>
2022-07-16 01:03:10 +1000

    setting favicon

--- src/components/js/Main.js
+++ src/components/js/Main.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router} from 'react-router-dom';
+import Favicon from 'react-favicon'
+import favicon from '../images/news-16x16.png';
 import Header from './base/Header';
 import Body from './base/Body';
 import Footer from './base/Footer';
@@ -7,6 +9,7 @@ import '../scss/index.scss';
 function Main() {
     return (
         <Router>
+            <Favicon url={favicon}/>
             <Header/>
             <Body/>
             <Footer/>
